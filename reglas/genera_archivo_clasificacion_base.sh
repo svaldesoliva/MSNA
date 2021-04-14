e126aae9338a6b5b3c912f7e0056344aa91730a8
manuel cano <manuel.cano@agenciaeducacion.cl>
2021-04-13 21:37:12 -0400

    script de extraccion de reglas

---
+++ reglas/genera_archivo_clasificacion_base.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#
+# Extrae las reglas de cada archivo
+# Manuel Cano - manuelcanoo@gmail.com
+#
+
+SALIDA="../clasificacion_base.csv"
+
+#tipo de salto de linea
+TSL=$'\n'
+cd rules
+rm -f $SALIDA
+echo "SID; ID_Clasificacion; nombre_clasificacion; archivo; alerta" > $SALIDA
+for f in *.rules ; do
+	echo -n "Procesando: $f"
+	#listado de alerta de in archivo, ie filas que no sean comentarios
+	ALERTAS_ARCHIVO=`grep 'alert' $f|grep -v '# alert'|grep 'sid:'`
+	#lectura linea a linea dentro de la variable, un "for" separaba por espacios
+	while TSL= read -r ALERTA; do
+		echo -n "."
+		SID=`echo -E $ALERTA|sed 's/\;/\n/g'|grep 'sid:'|cut -d ':' -f 2`
+		ALERTA_SEGURA=`echo -E $ALERTA|sed 's/\;/,/g'|sed 's/\n/,/g'|sed 's/\r/,/g'`
+		if [[ ! -z "$SID" ]] ; then # si no es vacio
+			echo -E "$SID;;; $f; $ALERTA_SEGURA" >> $SALIDA
+		fi
+	done <<< "$ALERTAS_ARCHIVO"
+	echo ""
+done
+
