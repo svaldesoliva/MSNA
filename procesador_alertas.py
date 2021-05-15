41bca59eeb381c71ac30de6466d1fc7bf4081d6b
manuel cano <manuel.cano@agenciaeducacion.cl>
2021-05-15 13:34:37 -0400

    comentarios, manejo de errores, servicio

--- procesador_alertas.py
+++ procesador_alertas.py
@@ -6,10 +6,14 @@
 #
 
 # Configuración
-archivoAlertas = "alert.csv"
-#archivoAlertas = "/var/log/snort/alert.csv"
+archivoAlertas = "alert.csv"  # Copia local de alertas
+#archivoAlertas = "/var/log/snort/alert.csv"  # Alertas en directorio donde se genera, para lectura en linea
 
 reglasClasificacion = "clasificacion.csv"
+
+# Si es servicio: leerá todo el archivo la primera vez, y lo restante cuando se ejecute de nuevo. Queda en loop. 
+#    No         : leerá desde el inicio cada vez.
+servicio=False 
 
 # Software
 import pandas as pd
@@ -24,15 +28,22 @@ def start():
 	# Carga inicial de clasificacion de reglas 
 	clasificacion = pd.read_csv(reglasClasificacion,encoding="latin-1",sep=";")
 
-	# Durante el desarrollo, reiniciamos la lectura de las alertas
-	if os.path.exists("alert.csv.offset"):
-	    os.remove("alert.csv.offset")
+	# Si no es servicio, reiniciamos la lectura de las alertas
+	if !servicio && os.path.exists( archivoAlertas + ".offset"):
+	    os.remove( archivoAlertas + ".offset")
 
-	# Leer linea a linea en la medida que aparezcan, como un servicio. Pygtail solo lee lineas (alertas) nuevas
+	if servicio
+		while True:
+			run()
+			time.sleep(60)
+	else
+		run()
+
+def run():
+	# Leer linea a linea. Pygtail solo lee lineas (alertas) nuevas
 	for linea in Pygtail( archivoAlertas ):
 	    alerta=lib.carga.separa(linea)
 	    alertaClasificada=lib.carga.clasifica(alerta, clasificacion)
-
 
 if __name__ == '__main__':
     start()
