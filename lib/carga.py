999e7ef4bdde83ac3a47d835bdfd20a8839533fc
manuel cano <manuel.cano@agenciaeducacion.cl>
2021-05-15 13:27:49 -0400

    modificaciones carga

--- lib/carga.py
+++ lib/carga.py
@@ -2,20 +2,31 @@ import pandas as pd
 
 
 def separa(linea):
+	# limpieza previa
 	linea = linea.strip()
 	linea = linea.strip('\n')
 	linea = linea.strip('\t')
 	linea = linea.strip('\r')
+	# separación en campos
 	fila = linea.split(",")
 	return(fila)
 
-def clasifica(linea):
-#    print(linea)
-	linea = linea.strip()
-	linea = linea.strip('\n')
-	linea = linea.strip('\t')
-	linea = linea.strip('\r')
-	fila = linea.split(",")
+
+def clasifica(alerta, clasificacion):
+	# alerta_csv: timestamp,sig_generator,sig_id,sig_rev,msg,proto,src,srcport,dst,dstport,ethsrc,ethdst,ethlen,tcpflags,tcpseq,tcpack,tcpln,tcpwindow,ttl,tos,id,dgmlen,iplen,icmptype,icmpcode,icmpid,icmpseq
+	# https://github.com/threatstream/snort/blob/master/src/output-plugins/spo_csv.c
+	#
+	# clasificacion: SID, Etapa, Subetapa, Observaciones, archivo, alerta
+
+
 	#matriz_de_datos.append(fila)
-	#print(fila[5])
-	return(fila)
+	print(alerta[2] + " " + alerta[6] )
+	#print(clasificacion[alerta[2]]) 
+
+
+	#for row in clasificacion:
+	#	for element in row:
+	#		if element == alerta[2]:
+	#			print(element)
+
+	return(alerta)
--- procesador_alertas.py
+++ procesador_alertas.py
@@ -12,16 +12,26 @@ archivoAlertas = "alert.csv"
 reglasClasificacion = "clasificacion.csv"
 
 # Software
+import pandas as pd
 from pygtail import Pygtail
 import lib.carga
+import os
 
 __version__ = '0.1'
 matriz_de_datos = []
 
 def start():
+	# Carga inicial de clasificacion de reglas 
+	clasificacion = pd.read_csv(reglasClasificacion,encoding="latin-1",sep=";")
+
+	# Durante el desarrollo, reiniciamos la lectura de las alertas
+	if os.path.exists("alert.csv.offset"):
+	    os.remove("alert.csv.offset")
+
+	# Leer linea a linea en la medida que aparezcan, como un servicio. Pygtail solo lee lineas (alertas) nuevas
 	for linea in Pygtail( archivoAlertas ):
-	    fila=lib.carga.separa(linea)
-	    clasificacion=lib.carga.clasifica(fila)
+	    alerta=lib.carga.separa(linea)
+	    alertaClasificada=lib.carga.clasifica(alerta, clasificacion)
 
 
 if __name__ == '__main__':
