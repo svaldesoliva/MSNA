c0b4d11d43190f788b56d254cc2bf04482cb8975
manuel cano <manuel.cano@agenciaeducacion.cl>
2021-05-15 18:20:00 -0400

    commit anterior con error en syntaxis :S

--- procesador_alertas.py
+++ procesador_alertas.py
@@ -7,12 +7,14 @@
 
 # Configuración
 archivoAlertas = "alert.csv"  # Copia local de alertas
-#archivoAlertas = "/var/log/snort/alert.csv"  # Alertas en directorio donde se genera, para lectura en linea
+#archivoAlertas = "/var/log/snort/alert.csv"  # Alertas en directorio donde se genera (linux), para lectura en linea
 
-reglasClasificacion = "clasificacion.csv"
+reglasClasificacion = "clasificacion.csv" #Archivo con reglas clasificadas
 
-# Si es servicio: leerá todo el archivo la primera vez, y lo restante cuando se ejecute de nuevo. Queda en loop. 
-#    No         : leerá desde el inicio cada vez.
+# ¿es Servicio ?	
+# 	True:	leerá todo el archivo la primera vez, y lo restante cuando se ejecute de nuevo. 
+#			Queda en loop. 
+#   False:	leerá desde el inicio cada vez. sin loop
 servicio=False 
 
 # Software
@@ -22,25 +24,23 @@ import lib.carga
 import os
 
 __version__ = '0.1'
-matriz_de_datos = []
+##matriz_de_datos = []
 
 def start():
 	# Carga inicial de clasificacion de reglas 
 	clasificacion = pd.read_csv(reglasClasificacion,encoding="latin-1",sep=";")
-
 	# Si no es servicio, reiniciamos la lectura de las alertas
-	if !servicio && os.path.exists( archivoAlertas + ".offset"):
+	if not servicio and os.path.exists( archivoAlertas + ".offset"):
 	    os.remove( archivoAlertas + ".offset")
-
-	if servicio
+	if servicio:
 		while True:
-			run()
+			run(clasificacion)
 			time.sleep(60)
-	else
-		run()
+	else:
+		run(clasificacion)
 
-def run():
-	# Leer linea a linea. Pygtail solo lee lineas (alertas) nuevas
+def run(clasificacion):
+	# Leer y procesa linea a linea. Pygtail solo lee lineas (alertas) nuevas
 	for linea in Pygtail( archivoAlertas ):
 	    alerta=lib.carga.separa(linea)
 	    alertaClasificada=lib.carga.clasifica(alerta, clasificacion)
