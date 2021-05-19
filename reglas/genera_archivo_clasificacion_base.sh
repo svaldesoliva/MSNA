759ab721da60acf0b9da7d84e519638eaa4b07fd
manuel cano <manuel.cano@agenciaeducacion.cl>
2021-05-19 18:42:17 -0400

    nuevo generador base, que incluye información acerca de que ip es local y cual remota, esta información será necesaria en el procesamiento

--- reglas/genera_archivo_clasificacion_base.sh
+++ reglas/genera_archivo_clasificacion_base.sh
@@ -11,7 +11,7 @@ SALIDA="../clasificacion_base.csv"
 TSL=$'\n'
 cd rules
 rm -f $SALIDA
-echo "SID; Etapa; Subetapa; Observaciones; archivo; alerta" > $SALIDA
+echo "SID; Etapa; Subetapa; Observaciones; tipo_destino; archivo; alerta" > $SALIDA
 for f in *.rules ; do
 	echo -n "Procesando: $f"
 	#listado de alerta de in archivo, ie filas que no sean comentarios
@@ -23,10 +23,20 @@ for f in *.rules ; do
 		echo -n "."
 		SID=`echo -E $ALERTA|sed 's/\;/\n/g'|grep 'sid:'|cut -d ':' -f 2`
 		ALERTA_SEGURA=`echo -E $ALERTA|sed 's/\;/,/g'|sed 's/\n/,/g'|sed 's/\r/,/g'`
+		DESTINO=`echo $ALERTA_SEGURA|cut -d '>' -f 2|cut -d ' ' -f 2|cut -d '$' -f 2`
+		DESTINO2=`echo $ALERTA_SEGURA|cut -d '>' -f 2|cut -d ' ' -f 2`
+		CODIGODESTINO=0
+		if [[ "$DESTINO" == "HOME_NET" ]] || [[ "$DESTINO" == "TELNET_SERVERS" ]] || [[ "$DESTINO" == "SMTP_SERVERS" ]] || [[ "$DESTINO" == "HTTP_SERVERS" ]] || [[ "$DESTINO" == "SQL_SERVERS" ]] || [[ "$DESTINO" == "any" ]]; then # si no es vacio
+			CODIGODESTINO=1
+		fi
+		if [[ "$DESTINO" == "EXTERNAL_NET" ]] ; then # si no es vacio
+			CODIGODESTINO=2
+		fi
+		#echo "($DESTINO)($DESTINO2) ($CODIGODESTINO) - $SID - $ALERTA_SEGURA)"
 		if [[ ! -z "$SID" ]] ; then # si no es vacio
-			echo -E "$SID;;;; $f; $ALERTA_SEGURA" >> $SALIDA
+			echo -E "$SID;;;;$CODIGODESTINO;$f;$ALERTA_SEGURA" >> $SALIDA
 		fi
 	done <<< "$ALERTAS_ARCHIVO"
-	echo " [$COUNTER]"
+	echo " [$COUNTER reglas]"
 done
 
