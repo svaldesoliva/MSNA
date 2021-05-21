a4125d458eba636a5e1733883775062417868b9a
manuel cano <manuel.cano@agenciaeducacion.cl>
2021-05-20 23:05:07 -0400

    cambios de origen/destino por local/remoto barra de progreso, ahora sabemos que si avanza

--- procesador_alertas.py
+++ procesador_alertas.py
@@ -25,9 +25,12 @@ from pygtail import Pygtail
 import lib.carga
 import lib.procesa
 import os
+from progress.bar import Bar
+import matplotlib.pyplot as plt
+
 
 __version__ = '0.1'
-##matriz_de_datos = []
+
 
 def start():
 	"""
@@ -41,11 +44,11 @@ def start():
 	
 	"""
 	# Indicadores, inicial vacío
-	indicadores_atacantes = pd.DataFrame(columns=('Origen', 'Etapa', 'contador'))
-	indicadores_hosts = pd.DataFrame(columns=('Destino', 'Etapa', 'contador'))
-	indicadores_detalle = pd.DataFrame(columns=('Origen', 'Destino', 'Etapa', 'contador'))
+	indicadores_atacantes = pd.DataFrame(columns=('Remoto', 'Etapa', 'contador'))
+	indicadores_hosts = pd.DataFrame(columns=('Local', 'Etapa', 'contador'))
+	indicadores_detalle = pd.DataFrame(columns=('Remoto', 'Local', 'Etapa', 'contador'))
 	# Registro general de alertas ya clasificadas, inicial vacío
-	repositorioAlertasClasificadas = pd.DataFrame(columns=('timestamp','SID','Etapa','Subetapa','Origen','Destino'))
+	repositorioAlertasClasificadas = pd.DataFrame(columns=('timestamp','SID','Etapa','Subetapa','Remoto','Local'))
 	# Carga inicial de clasificacion de reglas 
 	clasificacion = pd.read_csv(reglasClasificacion,encoding="latin-1",sep=";")
 	lib.carga.crear_archivo_sid_sin_clasificar()
@@ -80,8 +83,13 @@ def run(repositorioAlertasClasificadas, clasificacion, indicadores_atacantes, in
 	Returns
 	-------
 	
-	"""
-	# Leer y procesa linea a linea. Pygtail solo lee lineas (alertas) nuevas
+	""
+	if not servicio:
+		f = open(archivoAlertas, 'r')
+		bar1 = Bar('Procesando:', max=len(f.readlines()))
+		f.close()
+
+	# Leer y procesa linea a linea. Pygtail solo lee lineas (alertas) nuevas	
 	for linea in Pygtail( archivoAlertas ):
 	    alerta=lib.carga.separa(linea)
 	    # Clasificar tipo (1,2,3,4); o null/vacia: si no es de interes o es imposible de clasificar
@@ -91,10 +99,50 @@ def run(repositorioAlertasClasificadas, clasificacion, indicadores_atacantes, in
 	    	indicadores_atacantes, indicadores_hosts, indicadores_detalle = lib.procesa.generaIndicadores(alertaClasificada, 
 	    							indicadores_atacantes, indicadores_hosts, indicadores_detalle)
 
+	    if not servicio:
+	    	bar1.next()
+
+		# Fin FOR
+
+	if not servicio:
+		bar1.finish()
+
 	 # Operacion final
+	indicadores_atacantes.sort_values(['Remoto', 'Etapa'], ascending=[True, True])
 	indicadores_atacantes.to_csv("indicadores_atacantes.csv",encoding="latin-1",sep=";", index=False)	
+
+	indicadores_hosts.sort_values(['Local', 'Etapa'], ascending=[True, True])
 	indicadores_hosts.to_csv("indicadores_hosts.csv",encoding="latin-1",sep=";", index=False)
+
+	indicadores_detalle.sort_values(['Remoto', 'Local', 'Etapa'], ascending=[True, True, True])
 	indicadores_detalle.to_csv("indicadores_detalle.csv",encoding="latin-1",sep=";", index=False)
+
+	"""
+	indicadores_atacantes = pd.read_csv("indicadores_atacantes.csv",encoding="latin-1",sep=";")
+	indicadores_hosts = pd.read_csv("indicadores_hosts.csv",encoding="latin-1",sep=";")
+	indicadores_detalle = pd.read_csv("indicadores_detalle.csv",encoding="latin-1",sep=";")
+
+#https://www.delftstack.com/es/howto/matplotlib/pandas-plot-multiple-columns-on-bar-chart-matplotlib/
+#http://bl.ocks.org/ndarville/7075823
+
+	l1=indicadores_hosts.query("Local == '192.168.100.128'")
+
+
+	hosts=["192.168.100.128"]
+	ataques={
+	    "Ataques 1":[10],
+	    "2":[20],
+	    "3":[18],
+	    "4":[18],
+	}
+
+	df=pd.DataFrame(ataques,index=hosts)
+#	df.DataFrame({	'SID': sid }, index=[0])
+
+	df.plot(kind="bar",stacked=True,figsize=(10,8))
+	plt.legend(loc="lower left",bbox_to_anchor=(0.8,1.0))
+	plt.show()	
+
 
 if __name__ == '__main__':
     start()
