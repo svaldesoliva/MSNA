e9d55f1b4cf76fb2e84431164ac6117c21489ce7
manuel cano <manuel.cano@agenciaeducacion.cl>
2021-06-04 09:07:04 -0400

    permite generar graficos sin tener que reprocesar todo, ahorra tiempo

--- 03 procesador alertas/procesador_alertas.py
+++ 03 procesador alertas/procesador_alertas.py
@@ -22,6 +22,9 @@ servicio=False
 # Carpeta web en donde se realizará la entrega de los datos
 carpetaSalida = "salida/" #"html/data/"
 
+
+SoloGraficas=False
+
 #################################################################
 # Software
 import pandas as pd
@@ -87,54 +90,60 @@ def run(repositorioAlertasClasificadas, clasificacion, indicadores_atacantes, in
 	-------
 	
 	"""
-	if not servicio: #Leemos la cantidad de lineas para ver el avance
-		f = open(archivoAlertas, 'r')
-		operaciones=len(f.readlines())
-		f.close()
-		bar1 = Bar('Procesando:', max=operaciones, suffix = ' %(index)d/%(max)d - remanente %(eta)ds ')
-
-	# Leer y procesa linea a linea. Pygtail solo lee lineas (alertas) nuevas	
-	for linea in Pygtail( archivoAlertas ):
-	    alerta=lib.carga.separa(linea)
-	    # Clasificar tipo (1,2,3,4); o null/vacia: si no es de interes o es imposible de clasificar
-	    alertaClasificada=lib.carga.clasifica(alerta, clasificacion)
-	    if ( not alertaClasificada is None ) and ( len(alertaClasificada.index) > 0 ): # Se procesa solo si no viene vacía
-	    	##repositorioAlertasClasificadas.append(alertaClasificada, ignore_index=True)
-	    	## 'timestamp','SID','Etapa','Subetapa','Remoto','Local'
-	    	repositorioAlertasClasificadas.loc[len(repositorioAlertasClasificadas.index)] = [alertaClasificada["timestamp"].item(), alertaClasificada["SID"].item(), alertaClasificada["Etapa"].item(), alertaClasificada["Subetapa"].item(), alertaClasificada["Remoto"].item(), alertaClasificada["Local"].item()]
-
-	    	indicadores_atacantes, indicadores_hosts, indicadores_detalle = lib.procesa.generaIndicadores(alertaClasificada, 
-	    							indicadores_atacantes, indicadores_hosts, indicadores_detalle)
-
-	    if not servicio:
-	    	bar1.next()
-
-		# Fin FOR
-
-	if not servicio:
-		bar1.finish()
-	#""
-	# Operacion final
-	repositorioAlertasClasificadas.to_csv(carpetaSalida + "alertas_clasificadas.csv",encoding="latin-1",sep=";", index=False)
-
-	indicadores_atacantes.sort_values(['Etapa 1'], ascending=[False])
-	indicadores_atacantes.to_csv(carpetaSalida + "indicadores_atacantes.csv",encoding="latin-1",sep=";", index=False)	
-
-	indicadores_hosts.sort_values(['Etapa 1'], ascending=[False])
-	indicadores_hosts.to_csv(carpetaSalida + "indicadores_hosts.csv",encoding="latin-1",sep=";", index=False)
-
-	indicadores_detalle.sort_values(['Etapa 1'], ascending=[False])
-	indicadores_detalle.to_csv(carpetaSalida + "indicadores_detalle.csv",encoding="latin-1",sep=";", index=False)
-
-	if not servicio:
-		print(indicadores_atacantes)
-		print(indicadores_hosts)
-		print(indicadores_detalle)
-	#""
-
-	#https://www.delftstack.com/es/howto/matplotlib/pandas-plot-multiple-columns-on-bar-chart-matplotlib/
-	#http://bl.ocks.org/ndarville/7075823
-
+	if ( SoloGraficas == False ):
+		if not servicio: #Leemos la cantidad de lineas para ver el avance
+			f = open(archivoAlertas, 'r')
+			operaciones=len(f.readlines())
+			f.close()
+			bar1 = Bar('Procesando:', max=operaciones, suffix = ' %(index)d/%(max)d - remanente %(eta)ds ')
+		contadorGrupo = 0
+		# Leer y procesa linea a linea. Pygtail solo lee lineas (alertas) nuevas	
+		for linea in Pygtail( archivoAlertas ):
+			alerta=lib.carga.separa(linea)
+			# Clasificar tipo (1,2,3,4); o null/vacia: si no es de interes o es imposible de clasificar
+			alertaClasificada=lib.carga.clasifica(alerta, clasificacion, repositorioAlertasClasificadas)
+			if ( not alertaClasificada is None ) and ( len(alertaClasificada.index) > 0 ): # Se procesa solo si no viene vacía
+				##repositorioAlertasClasificadas.append(alertaClasificada, ignore_index=True)
+				## 'timestamp','SID','Etapa','Subetapa','Remoto','Local'
+				repositorioAlertasClasificadas.loc[len(repositorioAlertasClasificadas.index)] = [alertaClasificada["timestamp"].item(), alertaClasificada["SID"].item(), alertaClasificada["Etapa"].item(), alertaClasificada["Subetapa"].item(), alertaClasificada["Remoto"].item(), alertaClasificada["Local"].item()]
+				indicadores_atacantes, indicadores_hosts, indicadores_detalle = lib.procesa.generaIndicadores(alertaClasificada, indicadores_atacantes, indicadores_hosts, indicadores_detalle)
+				if servicio: #Guarda avance, genera indicadores graficos x grupo para mostrar avance 
+					contadorGrupo = contadorGrupo + 1
+					if ( contadorGrupo > 2000 ):
+						contadorGrupo = 0
+						repositorioAlertasClasificadas.to_csv(carpetaSalida + "alertas_clasificadas.csv",encoding="latin-1",sep=";", index=False)
+						indicadores_atacantes.sort_values(['Etapa 1'], ascending=[False])
+						indicadores_atacantes.to_csv(carpetaSalida + "indicadores_atacantes.csv",encoding="latin-1",sep=";", index=False)	
+						indicadores_hosts.sort_values(['Etapa 1'], ascending=[False])
+						indicadores_hosts.to_csv(carpetaSalida + "indicadores_hosts.csv",encoding="latin-1",sep=";", index=False)
+						indicadores_detalle.sort_values(['Etapa 1'], ascending=[False])
+						indicadores_detalle.to_csv(carpetaSalida + "indicadores_detalle.csv",encoding="latin-1",sep=";", index=False)
+						lib.procesa.generaGraficos( "indicadores_atacantes.csv", "indicadores_hosts.csv",  "indicadores_detalle.csv", "alertas_clasificadas.csv", carpetaSalida )
+			#Fin Si (Se procesa solo si no viene vacía)
+			if not servicio:
+				bar1.next()
+		# Fin FOR / ciclo terminado
+		if not servicio:
+			bar1.finish()
+		# Proceso final de indicadores
+		repositorioAlertasClasificadas.to_csv(carpetaSalida + "alertas_clasificadas.csv",encoding="latin-1",sep=";", index=False)
+		indicadores_atacantes.sort_values(['Etapa 1'], ascending=[False])
+		indicadores_atacantes.to_csv(carpetaSalida + "indicadores_atacantes.csv",encoding="latin-1",sep=";", index=False)	
+		indicadores_hosts.sort_values(['Etapa 1'], ascending=[False])
+		indicadores_hosts.to_csv(carpetaSalida + "indicadores_hosts.csv",encoding="latin-1",sep=";", index=False)
+		indicadores_detalle.sort_values(['Etapa 1'], ascending=[False])
+		indicadores_detalle.to_csv(carpetaSalida + "indicadores_detalle.csv",encoding="latin-1",sep=";", index=False)
+		if not servicio: #Mostramos por pantalla
+			print("indicadores atacantes")
+			print(indicadores_atacantes)
+			print("indicadores hosts")
+			print(indicadores_hosts)
+			print("indicadores detalle")
+			print(indicadores_detalle)
+		#""
+		#https://www.delftstack.com/es/howto/matplotlib/pandas-plot-multiple-columns-on-bar-chart-matplotlib/
+		#http://bl.ocks.org/ndarville/7075823
+	# Fin / if SoloGraficas
 	if not servicio:
 		print("Generando graficos")
 	lib.procesa.generaGraficos( "indicadores_atacantes.csv", "indicadores_hosts.csv",  "indicadores_detalle.csv", "alertas_clasificadas.csv", carpetaSalida )
